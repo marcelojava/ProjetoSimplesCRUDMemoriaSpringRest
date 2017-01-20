cdef363013ec73a9d68f7df6c40412a5f129f088
marcelo dos santos silva <marcelo.iron09@gmail.com>
2017-01-20 18:08:01 -0200

    add dependencias do spring data com mongo e a configuaracao do mongo

---
+++ src/main/java/br/com/pacote/config/MongoConfig.java
@@ -0,0 +1,29 @@
+package br.com.pacote.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "br.com.pacote.repositories")
+public class MongoConfig  extends AbstractMongoConfiguration{
+
+	@Override
+	protected String getDatabaseName() {
+		return "test";
+	}
+
+	@Override
+	public Mongo mongo() throws Exception {
+		return new MongoClient("127.0.0.1", 27017);
+	}
+	
+	@Override
+    protected String getMappingBasePackage() {
+        return "br.com.pacote";
+    }
+
+}
---
+++ src/main/java/br/com/pacote/repositories/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package br.com.pacote.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProdutoRepository{
+
+}
