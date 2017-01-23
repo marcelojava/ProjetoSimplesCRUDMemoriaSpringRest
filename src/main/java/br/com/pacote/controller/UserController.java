8a4d8afc104b93878857655ec377eee8684ab61d
marcelo dos santos silva <marcelo.iron09@gmail.com>
2017-01-23 15:15:50 -0200

    add metodo para criar um usuario

--- src/main/java/br/com/pacote/controller/UserController.java
+++ src/main/java/br/com/pacote/controller/UserController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,11 @@ public class UserController {
 		List<UserDTO> userDtos = users.stream().map(UserDTO::new).collect(toList());
 
 		return new ResponseEntity<>(userDtos, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "user", method = RequestMethod.POST)
+	public ResponseEntity<Void> save(@RequestBody User user) {
+		this.userServico.save(user);
+		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 }
