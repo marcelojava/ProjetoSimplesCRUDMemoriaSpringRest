2ab9ea2de4ea5867b71153615c35e7e0e4628a81
marcelo dos santos silva <marcelo.iron09@gmail.com>
2017-01-23 14:56:41 -0200

    logando a aplicacao com log4j e criando dto para user

--- src/main/java/br/com/pacote/controller/UserController.java
+++ src/main/java/br/com/pacote/controller/UserController.java
@@ -1,5 +1,7 @@
 package br.com.pacote.controller;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.pacote.dto.UserDTO;
 import br.com.pacote.modelo.User;
 import br.com.pacote.servico.UserServico;
 
@@ -19,12 +22,14 @@ public class UserController {
 	private UserServico userServico;
 
 	@RequestMapping(value = "user", method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = this.userServico.findAll();
 
 		if (users.isEmpty())
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		
+		List<UserDTO> userDtos = users.stream().map(UserDTO::new).collect(toList());
 
-		return new ResponseEntity<>(users, HttpStatus.OK);
+		return new ResponseEntity<>(userDtos, HttpStatus.OK);
 	}
 }
--- src/main/java/br/com/pacote/servico/UserServico.java
+++ src/main/java/br/com/pacote/servico/UserServico.java
@@ -2,6 +2,7 @@ package br.com.pacote.servico;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
@@ -11,11 +12,19 @@ import br.com.pacote.modelo.User;
 @Service
 public class UserServico {
 	
+	private  final static Logger logger = Logger.getLogger(UserServico.class);
+	
 	@Autowired
 	private MongoOperations mongoOperations;
 	
 	public List<User> findAll() {
+		logger.info("Buscando usuario na base");
 		return this.mongoOperations.findAll(User.class);
+	}
+	
+	public void save(User user) {
+		logger.info("salvando novo usuario na base");
+		this.mongoOperations.save(user);
 	}
 
 }
